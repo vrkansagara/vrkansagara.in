6f8ba3fc15feaf3675a450a05f9480715bad6deb
Vallabh Kansagara <vallabh.kansagara@commercepundit.com>
2020-10-01 12:53:57 +0530

    removal of unwated keys

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -21,33 +21,6 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-
-// Setup SMTP transport
-        $transport = new SmtpTransport();
-        $options   = new SmtpOptions([
-            'name'              => 'smtp.sendgrid.net', //'smtp.mailtrap.io',
-            'host'              => 'smtp.sendgrid.net', //'smtp.mailtrap.io',
-            'port'              => 587,
-        'connection_class'  => 'login',
-        'connection_config' => [
-        'username' => 'apikey', //'d934fa534ee329',
-        'password' => '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', //'8456f4c9177b0b',
-            ],
-        ]);
-        $transport->setOptions($options);
-
-
-        $mail = new Message();
-        $mail->setBody('This is the text of the email......');
-        $mail->setFrom('vallabh@vrkansagara.in', 'VRKANSAGARA');
-        $mail->addTo('vrkansagara@gmail.com', 'Vallabh Kansagara');
-        $mail->setSubject('TestSubject');
-
-//        $transport = new Sendmail('-freturn_to_me@example.com');
-//        $transport->send($mail);
-
-        $transport->send($mail);
-
         return new ViewModel();
     }
 
