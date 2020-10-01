732a80f1777c91d2437146b61f5dc386c2212285
Vallabh Kansagara <vrkansagara@gmail.com>
2020-10-01 20:01:05 +0530

    srandom

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,55 @@
+const config = require('./webpack.config');
 const mix = require('laravel-mix');
+require('laravel-mix-eslint');
+function resolve(dir) {
+    return path.join(
+        __dirname,
+        '/resources/js',
+        dir
+    );
+}
+Mix.listen('configReady', webpackConfig => {
+    // Add "svg" to image loader test
+    const imageLoaderConfig = webpackConfig.module.rules.find(
+        rule =>
+            String(rule.test) ===
+            String(/(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/)
+    );
+    imageLoaderConfig.exclude = resolve('icons');
+});
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+// mix.webpackConfig(config);
+
 
 mix.js('resources/js/app.js', 'public/dist/js')
+    .extract([
+        'vue',
+        'axios',
+        'jquery',
+        'lodash'
+    ])
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('autoprefixer'),
+        ],
+    })
     .sass('resources/css/app.scss','public/dist/css', {
         implementation: require('node-sass'),
     })
 ;
+
+
+if (mix.inProduction()) {
+    // mix.version();
+} else {
+    if (process.env.LAMINAS_USE_ESLINT === 'true') {
+        mix.eslint();
+    }
+    // Development settings
+    mix
+        .sourceMaps()
+        .webpackConfig({
+            devtool: 'cheap-eval-source-map', // Fastest for development
+        });
+}
